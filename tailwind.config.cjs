80e6dcf777e6f95a2660bfeb676af857aa84a2d1
efemstf27 <efemstf27@gmail.com>
2026-08-20 14:57:42 +0300

    Upload tailwind.config.cjs

---
+++ tailwind.config.cjs
@@ -0,0 +1,16 @@
+module.exports = {
+  content: [
+    './index.html',
+    './src/**/*.{js,jsx,ts,tsx}'
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#0A0A14',
+        accent: '#7B61FF',
+        ghost: '#F0EFF4'
+      }
+    }
+  },
+  plugins: [],
+}
